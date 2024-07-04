b69c4f2a7679079094212355a1db640c6bd3284f
lexara-prime-ai <irfanghat@gmail.com>
2024-07-04 09:39:28 +0000

    Add build configuration for cxx

---
+++ src/blobstore.cc
@@ -0,0 +1,10 @@
+// src/blobstore.cc
+
+#include "../include/blobstore.h"
+
+BlobstoreClient::BlobstoreClient() {}
+
+std::unique_ptr<BlobstoreClient> new_blobstore_client() 
+{
+    return std::unique_ptr<BlobstoreClient>(new BlobstoreClient());
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main()
 {
-    printf("Rust interop incoming!");
-    printf("\n");
+    printf("Compilation successful!");
     std::cin.get();
 }
